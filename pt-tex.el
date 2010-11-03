bc7f859050ed9620752ee30fca644b237ff51c8d
pt <ptpttt@gmail.com>
2010-11-03 22:41:25 +0100

    remove defkey

--- pt-tex.el
+++ pt-tex.el
@@ -1,3 +1,4 @@
+(provide 'pt-tex)
 
 (eval-after-load "latex"
   '(progn (setq font-latex-script-display nil)
@@ -5,9 +6,11 @@
           (setq LaTeX-command "pdflatex --shell-escape --synctex=1")
           (add-hook 'LaTeX-mode-hook
                     '(lambda ()
-                       (defkey LaTeX-mode-map [?\C-c ?\C-c]
-                         (TeX-save-document (TeX-master-file))
-                         (TeX-command "LaTeX" 'TeX-master-file))
+                       (define-key LaTeX-mode-map [?\C-c ?\C-c]
+                         (lambda ()
+                           (interactive)
+                           (TeX-save-document (TeX-master-file))
+                           (TeX-command "LaTeX" 'TeX-master-file)))
 
                        (add-to-list 'TeX-view-program-list '("Open" ("open %o")))
                        (add-to-list 'TeX-view-program-selection '(output-pdf "Open"))))))
