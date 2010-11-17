b59ce24b50e5ec1105cce7d7e11757591eec86f8
pt <ptpttt@gmail.com>
2010-11-17 16:12:22 +0100

    add `pt-cc-output-file-name' and add the compilation commands to `pt-compile-run-alist'

--- pt-cc.el
+++ pt-cc.el
@@ -13,3 +13,22 @@
 (c-set-offset 'inline-open 0)
 (c-set-offset 'substatement-open 0)
 (c-set-offset 'block-open '+)
+
+(defun pt-cc-output-file-name (&optional file-name)
+  "Generate output file-name."
+  (setq file-name (or file-name (buffer-file-name)))
+  (when file-name
+    (let ((output (file-name-sans-extension file-name)))
+      (if (string= output (buffer-file-name))
+          (concat output ".out")
+        (if (memq system-type '(windows-nt ms-dos))
+            (concat output ".exe")
+          output)))))
+
+(add-to-list 'pt-compile-run-alist
+             `(c-mode . ("\\cgcc -Wall \"$(buffer-file-name)$\" -o \"$(pt-cc-output-file-name)$\""
+                         . ,(if (memq system-type '(ms-dos windows-nt))
+                                "START \"$(pt-cc-output-file-name)$\" CMD /K \"\"$(pt-cc-output-file-name)$\" & PAUSE & EXIT\""
+                              "\\n\\c$(pt-cc-output-file-name)$"))))
+;; (setq pt-compile-run-alist nil)
+(provide 'pt-cc)
