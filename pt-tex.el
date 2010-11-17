c49b9a44ee6e1b907a2efbd6e2786cf4b7da8e36
pt <ptpttt@gmail.com>
2010-11-17 16:14:22 +0100

    changed

--- pt-tex.el
+++ pt-tex.el
@@ -1,20 +1,17 @@
+(setq-default TeX-auto-untabify nil)
+(setq TeX-auto-untabify nil)
+
+(setq font-latex-script-display nil)
+(setq TeX-save-query nil)
+(setq LaTeX-command "pdflatex --shell-escape --synctex=1")
+(add-hook 'LaTeX-mode-hook
+          '(lambda ()
+             (define-key LaTeX-mode-map [?\C-c ?\C-c]
+               #'(lambda ()
+                   (interactive)
+                   (TeX-save-document (TeX-master-file))
+                   (TeX-command "LaTeX" 'TeX-master-file)))
+             (add-to-list 'TeX-view-program-list '("Open" ("open %o")))
+             (add-to-list 'TeX-view-program-selection '(output-pdf "Open"))))
+
 (provide 'pt-tex)
-
-(eval-after-load "latex"
-  '(progn (setq font-latex-script-display nil)
-          (setq TeX-save-query nil)
-          (setq LaTeX-command "pdflatex --shell-escape --synctex=1")
-          (add-hook 'LaTeX-mode-hook
-                    '(lambda ()
-                       (define-key LaTeX-mode-map [?\C-c ?\C-c]
-                         (lambda ()
-                           (interactive)
-                           (TeX-save-document (TeX-master-file))
-                           (TeX-command "LaTeX" 'TeX-master-file)))
-
-                       (add-to-list 'TeX-view-program-list '("Open" ("open %o")))
-                       (add-to-list 'TeX-view-program-selection '(output-pdf "Open"))))))
-
-(when (require 'tex-site nil t)
-  (setq-default TeX-auto-untabify nil)
-  (setq TeX-auto-untabify nil))
