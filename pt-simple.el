27dd6b3928da2ee61a9d6f7640372cdebccd95d9
Tao Peng <pt@taopeng.me>
2012-07-11 21:21:23 +0200

    Rename `pt-rename-this-buffer-and-file' which duplicates with `pt-rename-buffer-file'

--- pt-simple.el
+++ pt-simple.el
@@ -355,22 +355,6 @@ non-file-visted-buffer."
   (global-set-key [?\C-x ?\C-y] 'pt-pbpaste)
   (global-set-key [?\C-x ?\M-w] 'pt-pbcopy))
 
-;; from internet
-(defun pt-rename-this-file-and-buffer (new-name)
-  "Renames both current buffer and file it's visiting to NEW-NAME."
-  (interactive "sNew name: ")
-  (let ((name (buffer-name))
-        (filename (buffer-file-name)))
-    (if (not filename)
-        (message "Buffer '%s' is not visiting a file!" name)
-      (if (get-buffer new-name)
-          (message "A buffer named '%s' already exists!" new-name)
-        (progn
-          (rename-file name new-name 1)
-          (rename-buffer new-name)
-          (set-visited-file-name new-name)
-          (set-buffer-modified-p nil))))))
-
 (defun pt-delete-this-buffer-and-file ()
   "Removes file connected to current buffer and kills buffer."
   (interactive)
